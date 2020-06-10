0ad24f8a68fd85220295dde9939a09e809780a3a
guohaibing <guohaibing@yto.net.cn>
2020-06-10 09:28:11 +0800

    数据发布服务器

--- ghb-process/test_app_wsl.py
+++ ghb-process/test_app_wsl.py
@@ -1,13 +1,15 @@
+"""
+作者: 沈福利
+博客: https://blog.csdn.net/shenfuli
+联系QQ：1121025745
+"""
+from flask import Flask,request,jsonify
+
 import torch
-from flask import Flask, request, jsonify
-import models
-import transform
-
-import time
-from collections import OrderedDict
-
 from transform import transform_image
-
+import models
+from collections import OrderedDict
+import time
 app = Flask(__name__)
 
 import codecs
@@ -22,51 +24,46 @@ for line in codecs.open('../data/ImageNet1k_label.txt', 'r'):
     _name = _name.replace('\xa0', "")
     ImageNet_dict[int(_id)] = _name
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-
-device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-print('Pytorch facebookresearch-Ws_Images_resnext predict device =',device)
+print('Pytorch facebookresearch_WS-Images_resnext predict device = ',device)
 
 # 加载模型
 model_ft = models.resnext101_32x16d_wsl()
-# 预测问题，置顶eval
 model_ft.to(device)
-r = model_ft.eval()
-
+model_ft.eval() # 预测问题，指定eval
 
 @app.route('/')
 def hello():
-    return "hello world"
+    return "Hello World"
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # 获取输入数据
     file = request.files['file']
-    # 转换成字节
+
     img_bytes = file.read()
 
     # 数据预处理
-    image_tensor = transform_image(img_bytes = img_bytes)
-    print('image_tensor === ',image_tensor)
+    image_tensor  = transform_image(img_bytes = img_bytes)
     image_tensor = image_tensor.to(device)
 
     # 模型预测
+    # move the input and model to GPU for speed if available
     input_batch = image_tensor.to(device)
-
-
     with torch.no_grad():
         t1 = time.time()
         output = model_ft(input_batch)
         t2 = time.time()
-        consume = (t2-t1)*1000
+        consume = (t2 - t1) * 1000
         consume = int(consume)
 
     outputs = torch.nn.functional.softmax(output[0], dim=0)
-
+    # result -> list
     pred_list = outputs.cpu().numpy().tolist()
-    # API接口封装
+
+    # API 接口封装
     label_c_mapping = {}
     for i,prob in enumerate(pred_list):
         label_c_mapping[i] = prob
@@ -74,17 +71,15 @@ def predict():
     ## 获取topK=5 数据结果,按照dict 中prob 进行排序
     topK = 5
     data_list = []
-    for label_prob in sorted(label_c_mapping.items(), key=lambda x: x[1], reverse=True)[:topK]:
+    for label_prob in sorted(label_c_mapping.items(),key=lambda x:x[1],reverse=True)[:topK]:
         label = int(label_prob[0])
-        result = {'label': label, 'prob': label_prob[1], 'name': ImageNet_dict[label]}
+        result = {'label':label,'prob':label_prob[1],'name':ImageNet_dict[label]}
 
         data_list.append(result)
     # JSON 格式数据输出
-    result = OrderedDict(error=0, errmsg='success', consume=consume, data=data_list)
-    # Json 格式数据输出
-    # result = OrderedDict(error = 0,errmsg = 'success',consume = consume ,data = label_c_mapping)
-    return jsonify(result)
+    result = OrderedDict(error = 0,errmsg = 'success',consume = consume,data=data_list)
 
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run()
